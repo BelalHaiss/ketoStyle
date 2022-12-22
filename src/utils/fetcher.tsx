82feb09e620ffedc497670d54b1f9bdbc7bd6025
belal <belalhaiss@gmail.com>
2022-12-22 13:37:47 +0200

    edit axios default url

--- src/utils/fetcher.tsx
+++ src/utils/fetcher.tsx
@@ -1,9 +1,9 @@
-import axios from "axios";
-import Toast from "./Toast";
+import axios from 'axios';
+import Toast from './Toast';
 
 export type Args = {
   url: string;
-  method?: "get" | "post" | "patch" | "delete" | string;
+  method?: 'get' | 'post' | 'patch' | 'delete' | string;
   data?: any;
   successToast?: string;
   errorToast?: string; //mesage when error
@@ -16,21 +16,21 @@ let controller = new AbortController();
 let lastFetch: number;
 export const fetcher = async ({
   url,
-  method = "get",
+  method = 'get',
   data = null,
-  successToast = "", // text to display on success
+  successToast = '', // text to display on success
   errorToast,
   noErrorToast,
-  abortRequest = false,
+  abortRequest = false
 }: Args) => {
-  const devUrl = "http://localhost:5000/api";
-  // axios.defaults.baseURL = devUrl;
-  axios.defaults.baseURL = process.env.api || devUrl;
+  const devUrl = 'http://localhost:5000/api';
+  axios.defaults.baseURL = devUrl;
+  // axios.defaults.baseURL = process.env.api || devUrl;
   if (abortRequest) {
     const now = Date.now();
 
     if (now - lastFetch < 1000) {
-      controller.abort("multiple requests");
+      controller.abort('multiple requests');
       controller = new AbortController();
     }
     lastFetch = Date.now();
@@ -41,24 +41,24 @@ export const fetcher = async ({
       method,
       data,
       signal: controller.signal,
-      withCredentials: true,
+      withCredentials: true
     });
     if (successToast) {
-      Toast(successToast, "success");
+      Toast(successToast, 'success');
     }
 
     return res.data;
   } catch (e: any) {
-    if (e.message === "Network Error") {
-      Toast("حدث خطا بالاتصال في الشبكه", "error");
+    if (e.message === 'Network Error') {
+      Toast('حدث خطا بالاتصال في الشبكه', 'error');
       return;
     }
     if (noErrorToast) return null;
-    if (e.message === "canceled") return null;
-    if (e.response?.data?.name === "custom") {
-      Toast(e.response.data.message, "error");
+    if (e.message === 'canceled') return null;
+    if (e.response?.data?.name === 'custom') {
+      Toast(e.response.data.message, 'error');
     } else {
-      Toast(errorToast ?? "حدث خطا برجاء المحاولة لاحقا", "error");
+      Toast(errorToast ?? 'حدث خطا برجاء المحاولة لاحقا', 'error');
     }
     // if (abortRequest) return;
 
